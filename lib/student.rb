d170a544e81be275065ba60bb5c4e20b595c4411
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 05:26:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,12 @@
-class Student
-  attr_accessor :first_name, :last_name
-
+class < Student
+  def initialize
+    knowledge =[]
+  end
+  def learn(string_knowledge)
+    knowledge << string_knowledge
+  end
+  def knowledge
+    return knowledge
+  end
 end
+  
